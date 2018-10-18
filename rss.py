7a74f230b8840298154acaee4b2861ae962aa669
Ryan Chen <ryanjchen@yahoo.com>
2018-10-18 09:41:41 -0400

    It all works

--- rss.py
+++ rss.py
@@ -2,31 +2,36 @@ import feedparser
 import sqlite3
 import requests
 
-d = feedparser.parse("https://www.purdue.edu/newsroom/rss/general.xml")
+d = feedparser.parse("https://www.purdue.edu/newsroom/rss/academics.xml")
 
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
-# for x in d.entries:
-# 	try:
-# 		print str(x.title)
-# 		print str(x.link)
-# 		print str(x.published)
-# 		print str(x.summary)
-# 		print "\n"
+for x in d.entries:
+	print str(x.title)
+	print str(x.link)
+	print str(x.published)
+	print str(x.summary)
+	print "\n"
 
-# 		c.execute("insert into purdue_news values(?, ?, ?, ?)", (x.published, x.title, x.link, x.summary))
+	c.execute("select * from purdue_news where title=?", (x.title,))
 
-# 		pass
-# 	except Exception as e:
-# 		pass
+	if(not c.fetchone()):
+		payload = {'text':x.title}
+		requests.post("https://hooks.slack.com/services/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", json=payload)
 
-# conn.commit()
+	c.execute("insert or ignore into purdue_news(title, link, published, summary) values(? ,? ,? ,?)", (x.title, x.link, x.published, x.summary))
 
-for row in c.execute("select * from purdue_news"):
-	print row[1]
-	payload = {'text':row[1]}
-	requests.post("https://hooks.slack.com/services/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", json=payload)
+		# pass
+	# except Exception as e:
+	# 	pass
+
+conn.commit()
+
+# for row in c.execute("select * from purdue_news"):
+# 	print row[1]
+# 	payload = {'text':row[1]}
+# 	requests.post("https://hooks.slack.com/services/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", json=payload)
 
 
 
