46a8b6c772905163f5d389727027fe6779df95c6
Ryan Chen <ryanjchen@yahoo.com>
2018-10-18 10:23:11 -0400

    Upload stuff

--- rss.py
+++ rss.py
@@ -2,36 +2,35 @@ import feedparser
 import sqlite3
 import requests
 
-d = feedparser.parse("https://www.purdue.edu/newsroom/rss/academics.xml")
 
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
-for x in d.entries:
-	print str(x.title)
-	print str(x.link)
-	print str(x.published)
-	print str(x.summary)
-	print "\n"
+xml_links = ["http://www.purdue.edu/newsroom/rss/academics.xml", "http://www.purdue.edu/newsroom/rss/AdvNews.xml", "http://www.purdue.edu/newsroom/rss/AgriNews.xml"]
 
-	c.execute("select * from purdue_news where title=?", (x.title,))
+for link in xml_links:
+	d = feedparser.parse(link)
+	try:
+		for x in d.entries:
+			print str(x.title)
+			print str(x.link)
+			print str(x.published)
+			print str(x.summary)
+			print "\n"
 
-	if(not c.fetchone()):
-		payload = {'text':x.title}
-		requests.post("https://hooks.slack.com/services/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", json=payload)
+			c.execute("select * from purdue_news where title=?", (x.title,))
 
-	c.execute("insert or ignore into purdue_news(title, link, published, summary) values(? ,? ,? ,?)", (x.title, x.link, x.published, x.summary))
+			if(not c.fetchone()):
+				payload = {'text':x.title}
+				requests.post("https://hooks.slack.com/services/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", json=payload)
 
-		# pass
-	# except Exception as e:
-	# 	pass
+			c.execute("insert or ignore into purdue_news(title, link, published, summary) values(? ,? ,? ,?)", (x.title, x.link, x.published, x.summary))
+		pass
+	except:
+		pass
+
 
 conn.commit()
-
-# for row in c.execute("select * from purdue_news"):
-# 	print row[1]
-# 	payload = {'text':row[1]}
-# 	requests.post("https://hooks.slack.com/services/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", json=payload)
 
 
 
