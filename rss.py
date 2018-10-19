645dc67ee8b74d00b8a8418487d3609465ea0c74
Ryan Chen <ryanjchen@yahoo.com>
2018-10-19 14:53:27 -0400

    changes

--- rss.py
+++ rss.py
@@ -12,16 +12,22 @@ for link in xml_links:
 	d = feedparser.parse(link)
 	try:
 		for x in d.entries:
-			print str(x.title)
-			print str(x.link)
-			print str(x.published)
-			print str(x.summary)
-			print "\n"
-
+			print x
 			c.execute("select * from purdue_news where title=?", (x.title,))
-
-			if(not c.fetchone()):
-				payload = {'text':x.title}
+			listo = c.fetchall()
+			if(len(listo) != 1):
+				print x.title
+				payload = {
+					"attachments": [
+						{
+							"fallback": "Tippecanews!",
+							"color": "#36a64f",
+							"title": "Slack API Documentation",
+							"text": "Optional text that appears within the attachment",
+							"image_url": "https://akns-images.eonline.com/eol_images/Entire_Site/2018917/rs_634x826-181017125108-634-asap-rocky-esquire-101718.jpg"
+						}
+					]
+				}
 				requests.post("https://hooks.slack.com/services/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", json=payload)
 
 			c.execute("insert or ignore into purdue_news(title, link, published, summary) values(? ,? ,? ,?)", (x.title, x.link, x.published, x.summary))
