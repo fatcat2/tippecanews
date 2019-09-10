20e757fa5ed2e51901c9410a8d47fd9431acaa93
Ryan C <ryanjchen2@gmail.com>
2019-09-10 14:58:23 +0000

    Removed Python3.6

--- noxfile.py
+++ noxfile.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 import nox
 
 BLACK_PATHS = [
-        "app.py",
-        "info_getters.py"
+        "tippecanews",
+        "tests"
 ]
 
 
@@ -30,14 +30,13 @@ def blacken(session):
 def default(session):
     # Install all test dependencies, then install this package in-place.
     session.install("mock", "pytest", "pytest-cov", "pytest-asyncio")
-    # session.install("-e", ".")
     session.install("-r", "requirements.txt")
+    session.install("-e", ".")
     # Run py.test against the unit tests.
     session.run(
         "py.test",
         "--quiet",
-        # "--cov=util",
-        # "--cov=connector",
+        "--cov=tippecanews",
         "--cov-append",
         "--cov-config=.coveragerc",
         "--cov-report=",
@@ -47,18 +46,6 @@ def default(session):
     )
 
 
-@nox.session(python=["3.6", "3.7"])
+@nox.session(python=["3.7"])
 def unit(session):
     default(session)
-
-
-# @nox.session(python="3.7")
-# def cover(session):
-# """Run the final coverage report.
-# This outputs the coverage report aggregating coverage from the unit
-# test runs (not system test runs), and then erases coverage data.
-# """
-# session.install("coverage", "pytest-cov")
-# session.run("coverage", "report", "--show-missing", "--fail-under=100")
-
-# session.run("coverage", "erase")
