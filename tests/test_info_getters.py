3e909e598dead37d7acf474646678dc393795c00
Ryan Chen <ryanjchen2@gmail.com>
2019-09-12 09:52:03 -0400

    Added a test for valid xml_urls (#13)
    * Added a test for valid xml_urls
    
    * Added dummy test

---
+++ tests/test_info_getters.py
@@ -0,0 +1,21 @@
+import requests
+
+from tippecanews.info_getters import get_pngs, xml_urls
+
+
+def test_valid_xml_urls():
+    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "HIGH:!DH:!aNULL"
+    try:
+        requests.packages.urllib3.contrib.pyopenssl.DEFAULT_SSL_CIPHER_LIST += (
+            "HIGH:!DH:!aNULL"
+        )
+    except AttributeError:
+        pass
+
+    for url in xml_urls:
+        response = requests.get(url)
+        assert response.status_code == 200
+
+
+def test_get_pngs():
+    assert len(get_pngs()) >= 0
