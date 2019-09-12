71e23b5420cbf81ba88c65ed0b21888073ccb3fa
Ryan Chen <ryanjchen2@gmail.com>
2019-09-12 10:21:04 -0400

    Removed twitter from app.py (#15)
    * Removed twitter from app.py
    
    * Fixed lint error

--- tippecanews/app.py
+++ tippecanews/app.py
@@ -7,7 +7,6 @@ import json
 from google.cloud import firestore
 import requests
 from tippecanews.info_getters import xml_urls, get_pngs
-from tippecanews.twitter_helper import ryan_twtr_utils
 import logging
 
 app = Flask(__name__)
@@ -148,10 +147,6 @@ def newsfetch():
             )
         except Exception:
             pass
-
-    # Twitter actions
-    twtr = ryan_twtr_utils()
-    twtr.get_new_tweets()
 
     return "Done"
 
