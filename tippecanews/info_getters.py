e2872a1a3c389bf0c3c3b55d3e3ea0c333ff943b
Ryan Chen <ryanjchen2@gmail.com>
2019-09-11 22:24:40 -0400

    Add urls (#10)
    * Bridged gap between testing and deployment
    
    * initial commit
    
    * More urls
    
    * Black changes
    
    * Flake8 changes
    
    * Black changes
    
    * Get rid of a XML

--- tippecanews/info_getters.py
+++ tippecanews/info_getters.py
@@ -1,7 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
 
-xml_urls = ["http://www.purdue.edu/newsroom/rss/general.xml"]
+xml_urls = [
+    "http://www.purdue.edu/newsroom/rss/academics.xml",
+    "http://www.purdue.edu/newsroom/rss/AdvNews.xml",
+    "http://www.purdue.edu/newsroom/rss/AgriNews.xml",
+    "http://www.purdue.edu/newsroom/rss/BizNews.xml",
+    "http://www.purdue.edu/newsroom/rss/community.xml",
+    "http://www.purdue.edu/newsroom/rss/DiversityNews.xml",
+    "http://www.purdue.edu/newsroom/rss/EdCareerNews.xml",
+    "http://www.purdue.edu/newsroom/rss/EventNews.xml",
+    "http://www.purdue.edu/newsroom/rss/faculty_staff.xml",
+    "http://www.purdue.edu/newsroom/rss/FeaturedNews.xml",
+    "http://www.purdue.edu/newsroom/rss/general.xml",
+    "http://www.purdue.edu/newsroom/rss/HealthMedNews.xml",
+    "http://www.purdue.edu/newsroom/rss/hrnews.xml",
+    "http://www.purdue.edu/newsroom/rss/InfoTech.xml",
+    "http://www.purdue.edu/newsroom/rss/LifeNews.xml",
+    "http://www.purdue.edu/newsroom/rss/LifeSciNews.xml",
+    "http://www.purdue.edu/newsroom/rss/OTCNews.xml",
+    "http://www.purdue.edu/newsroom/rss/outreach.xml",
+    "http://www.purdue.edu/newsroom/rss/PhysicalSciNews.xml",
+    "http://www.purdue.edu/newsroom/rss/PRFAdminNews.xml",
+    "http://www.purdue.edu/newsroom/rss/ResearchNews.xml",
+    "http://www.purdue.edu/newsroom/rss/StudentNews.xml",
+    "http://www.purdue.edu/newsroom/rss/VetMedNews.xml",
+    "http://www.purdue.edu/newsroom/rss/AgNews.xml",
+    "http://www.purdue.edu/newsroom/rss/DiscoParkNews.xml",
+    "http://www.purdue.edu/newsroom/rss/EdNews.xml",
+    "http://www.purdue.edu/newsroom/rss/engineering.xml",
+    "http://www.purdue.edu/newsroom/rss/HHSNews.xml",
+    "http://www.purdue.edu/newsroom/rss/ITaPNews.xml",
+    "http://www.purdue.edu/newsroom/rss/CLANews.xml",
+    "http://www.purdue.edu/newsroom/rss/LibrariesNews.xml",
+    "http://www.purdue.edu/newsroom/rss/KrannertNews.xml",
+    "http://www.purdue.edu/newsroom/rss/NEESnews.xml",
+    "http://www.purdue.edu/newsroom/rss/NursingNews.xml",
+    "http://www.purdue.edu/newsroom/rss/PharmacyNews.xml",
+    "http://www.purdue.edu/newsroom/rss/president.xml",
+    "http://www.purdue.edu/newsroom/rss/PRFNews.xml",
+    "http://www.purdue.edu/newsroom/rss/ScienceNews.xml",
+    "http://www.purdue.edu/newsroom/rss/TechNews.xml",
+    "http://www.purdue.edu/newsroom/rss/VetNews.xml",
+]
 
 
 def get_pngs():
---
+++ tippecanews/twitter_helper.py
@@ -0,0 +1,102 @@
+import os
+from typing import List
+from dateutil import parser
+from datetime import datetime, timezone, timedelta
+import twitter
+import requests
+
+
+class ryan_twtr_utils:
+    _account_list = [
+        "WLFI",
+        "davebangert",
+        "JCOnline",
+        "TippecanoeCoSh1",
+        "AnnaDarlingTV",
+        "TrevorPetersTV",
+    ]
+
+    def __init__(self):
+        # Not assigning the following to this since we just want them once
+        consumer_key = os.getenv("twtr_consumer_key")
+        consumer_secret = os.getenv("twtr_consumer_secret")
+        access_token_key = os.getenv("twtr_access_token_key")
+        access_token_secret = os.getenv("twtr_access_token_secret")
+
+        # Creating the python-twitter API object
+        self.api = twitter.Api(
+            consumer_key=consumer_key,
+            consumer_secret=consumer_secret,
+            access_token_key=access_token_key,
+            access_token_secret=access_token_secret,
+        )
+
+    def _get_past_fifteen_mins(self, account: str) -> List[twitter.models.Status]:
+        """Function to get tweets from the past 15 minutes
+        :type account: str
+        :param account: A string representing a Twitter user's username.
+
+        :rtype: List[twitter.models.Status]
+        :returns: A list of tweets.
+        """
+        statuses = self.api.GetUserTimeline(screen_name=account)
+
+        ret_list = [
+            status
+            for status in statuses
+            if (
+                datetime.now(timezone.utc) - parser.parse(status.created_at)
+                < timedelta(minutes=15)  # noqa
+            )
+        ]
+        return ret_list
+
+    def _send_slack_twt(self, tweet: twitter.models.Status) -> None:
+        """Function to send tweets to a specified Slack channel. Requires the SLACK_TOKEN and SLACK_CHANNEL environment variables to be set.
+        :type tweet: twitter.models.Status
+        :param tweet: A Twitter status obtained using the python-twitter library.
+        """
+        headers = {"Authorization": "Bearer {}".format(os.getenv("SLACK_TOKEN"))}
+        payload = {
+            "channel": os.getenv("SLACK_CHANNEL"),
+            "text": tweet.text,
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text": f"{tweet.text}"},
+                    "accessory": {
+                        "type": "image",
+                        "image_url": f"{tweet.user.profile_image_url}",
+                        "alt_text": "alt text for image",
+                    },
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f"Posted on {tweet.created_at} by @{tweet.user.screen_name}",
+                        },
+                        {
+                            "type": "image",
+                            "image_url": "https://i.imgur.com/clhAX9Y.png",
+                            "alt_text": "images",
+                        },
+                    ],
+                },
+            ],
+        }
+
+        print(headers, payload)
+        r = requests.post(
+            "https://slack.com/api/chat.postMessage", headers=headers, json=payload
+        )
+        print(r)
+        r.raise_for_status()
+
+    def get_new_tweets(self) -> None:
+        """Function to get all new tweets from the account list.
+        """
+        for username in self._account_list:
+            for tweet in self._get_past_fifteen_mins(username):
+                self._send_slack_twt(tweet)
