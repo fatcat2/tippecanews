2aac22117fc955f41aa9d1e04e75571b2d3878e2
Ryan Chen <ryanjchen2@gmail.com>
2019-10-20 11:25:47 -0400

    more tests

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -17,3 +17,13 @@ def test_hello_world(client):
 def test_cms(client):
     resp = client.post("/cms")
     assert resp.status == "200 OK"
+
+
+def test_tcms(client):
+    resp = client.post("/tcms")
+    assert resp.status == "200 OK"
+
+
+def test_email(client):
+    resp = client.post("/email")
+    assert resp.status == "200 OK"
