acf97b8118d69a387f80ba9563f0fe5acd461a04
Ryan Chen <ryanjchen2@gmail.com>
2020-08-02 13:17:59 -0700

    naisu

---
+++ tippecanews/utils/processors.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+import re
+from typing import Any, Dict
+
+def process_bylines(entry_list) -> Dict[str, Any]:
+    bylines = defaultdict(lambda: {"articles": [], "count": 0})
+
+    regex_one = r"B([yY]) (\w+) (\w+)"
+    regex_two = r"B([yY]) (\w+) (\w+) AND (\w+) (\w+)"
+    regex_three = r"B([yY]) (\w+) (\w+), (\w+) (\w+) AND (\w+) (\w+)"
+
+    for entry in entry_list:
+        try:
+            entry["author"]
+        except:
+            continue
+        match_three = re.match(regex_three, entry["author"])
+        if match_three is None:
+            match_two = re.match(regex_two, entry["author"])
+            if match_two is None:
+                match_one = re.match(regex_one, entry["author"])
+                if match_one is None:
+                    print("Nothing found for: " + entry["author"])
+                    pass
+                else:
+                    key_string = f"{match_one.group(2)} {match_one.group(3)}"
+                    bylines[key_string]["articles"].append(entry.title)
+                    bylines[key_string]["count"] = bylines[key_string]["count"] + 1
+            else:
+                key_string = f"{match_two.group(2)} {match_two.group(3)}"
+                bylines[key_string]["articles"].append(entry.title)
+                bylines[key_string]["count"] = bylines[key_string]["count"] + 1
+
+                key_string = f"{match_two.group(4)} {match_two.group(5)}"
+                bylines[key_string]["articles"].append(entry.title)
+                bylines[key_string]["count"] = bylines[key_string]["count"] + 1
+        else:
+            key_string = f"{match_three.group(2)} {match_three.group(3)}"
+            bylines[key_string]["articles"].append(entry.title)
+            bylines[key_string]["count"] = bylines[key_string]["count"] + 1
+
+            key_string = f"{match_three.group(4)} {match_three.group(5)}"
+            bylines[key_string]["articles"].append(entry.title)
+            bylines[key_string]["count"] = bylines[key_string]["count"] + 1
+
+            key_string = f"{match_three.group(6)} {match_three.group(7)}"
+            bylines[key_string]["articles"].append(entry.title)
+            bylines[key_string]["count"] = bylines[key_string]["count"] + 1
+    
+    return bylines
