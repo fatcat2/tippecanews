651bfe6dae2b4a36be2b310e5a5af0702666f2a9
Ryan <ryanjchen2@gmai.com>
2020-09-26 09:51:29 -0400

    Fixed the send_matches function

--- tippecanews/utils/matches.py
+++ tippecanews/utils/matches.py
@@ -82,84 +82,24 @@ def send_matches() -> int:
     return counter
 
 
-def match_people():
+def match_people() -> int:
+    """A helper function to match pair people from the previous day together and create group chats.
+
+    Returns:
+        The number of pairs created.
+    """
     db = firestore.Client()
-    today = datetime.now()
-    day = datetime.now() - timedelta(days=datetime.now().weekday()+1)
+    
+    today = datetime.now() - timedelta(days=datetime.now().weekday()+1)
 
     week_doc = (
-        db.collection("meetings").document(f"{today.month}_{day}_{today.year}").get()
+        db.collection("meetings").document(f"{today.month}_{today.day}_{today.year}").get()
     )
+
     if week_doc.exists:
         members = week_doc.to_dict()["uids"]
-        random.shuffle(members)
-
-        pairs_list = []
         tmp = []
-        for uid in user_ids:
-            send_msg_params = {
-                "token": os.getenv("SLACK_TOKEN"),
-                "channel": uid,
-                "text": "Hey! Did you meet your assigned matched person this week?",
-            }
-
-            blocks = {
-                "type": "home",
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "Hey! Do you wanna meet someone new at The Exponent this week?",
-                        },
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "yes !",
-                                },
-                                "value": "met_the_person",
-                            },
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "no !",
-                                },
-                                "value": "didnt_meet_the_person",
-                            },
-                        ],
-                    },
-                ],
-            }
-
-            send_msg_params["blocks"] = json.dumps(blocks["blocks"])
-
-            r = requests.post(
-                "https://slack.com/api/chat.postMessage", params=send_msg_params
-            )
-
-
-        db.collection("meetings").document(
-            f"{today.month}_{today.day}_{today.year}"
-        ).update({"pairs": pairs_list})
-
-
-def check_matches():
-    if datetime.now() > 0:
-        day = datetime.now() - timedelta(days=datetime.now().weekday()+1)
-    else:
-        day = datetime.now()
-
-    week_doc = (
-        db.collection("meetings").document(f"{today.month}_{day}_{today.year}").get()
-    )
-    if week_doc.exists:
-        members = week_doc.to_dict()["uids"]
+        pairs_list = []
 
         for member in members:
             tmp.append(member)
@@ -187,6 +127,80 @@ def check_matches():
         db.collection("meetings").document(
             f"{today.month}_{today.day}_{today.year}"
         ).update({"pairs": pairs_list})
+
+
+def check_matches():
+    """Helper function to send the matches out to all members of the workspace.
+
+    Returns:
+        An int representing the number of users matching messages were sent to.
+    """
+    # get list of all users
+    list_users_url = "https://slack.com/api/users.list"
+
+    params = {"token": os.getenv("SLACK_TOKEN")}
+
+    r = requests.get(list_users_url, params=params)
+
+    data = r.json()
+
+    user_ids = [member["id"] for member in data["members"] if member["is_bot"] is False]
+
+    counter = 0
+
+    for uid in user_ids:
+        send_msg_params = {
+            "token": os.getenv("SLACK_TOKEN"),
+            "channel": uid,
+            "text": "Hey! Do you wanna meet someone new at The Exponent this week?",
+        }
+
+        blocks = {
+            "type": "home",
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Hey! Do you wanna meet someone new at The Exponent this week?",
+                    },
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "yes !",
+                            },
+                            "value": "yes",
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "no !",
+                            },
+                            "value": "no",
+                        },
+                    ],
+                },
+            ],
+        }
+
+        send_msg_params["blocks"] = json.dumps(blocks["blocks"])
+
+        r = requests.post(
+            "https://slack.com/api/chat.postMessage", params=send_msg_params
+        )
+
+        if r.json()["ok"]:
+            counter += 1
+        else:
+            raise Exception
+    
+    return counter
 
 
 if __name__ == "__main__":
