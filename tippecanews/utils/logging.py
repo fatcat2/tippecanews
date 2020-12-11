3f705566cf184c64c6c2efe2b0c4cfd59838582c
Ryan <ryanjchen2@gmail.com>
2020-12-10 22:43:57 -0800

    Changed library to use tippecanes library

--- tippecanews/utils/logging.py
+++ tippecanews/utils/logging.py
@@ -6,12 +6,28 @@ def log(message: str):
     print(f"{datetime.now()}: {message}")
 
 def log_request(endpoint: str="/"):
-    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'tippecanews')
     json_body = [
         {
             "measurement": "requests",
             "tags": {
                 "endpoint": endpoint,
+            },
+            "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "fields": {
+                "value": 1
+            }
+        }
+    ]
+    client.write_points(json_body)
+
+def log_error(function: str):
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'tippecanews')
+    json_body = [
+        {
+            "measurement": "errors",
+            "tags": {
+                "function": function,
             },
             "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
             "fields": {
