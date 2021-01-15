f99adfefef5d82388587098e06a5aac9a69ee85a
Ryan Chen <ryanjchen2@gmail.com>
2021-01-15 10:50:25 +0800

    Added database utility function

---
+++ tippecanews/utils/database.py
@@ -0,0 +1,27 @@
+import os
+import traceback
+
+from pg8000 import Connection, connect
+
+
+def get_database_connection() -> Connection:
+    """Helper function to get the database connection
+
+    Params:
+        database (str): the database you want to retrieve data from.
+
+    Returns:
+        A database connection object to interact with.
+    """
+    try:
+        conn = connect(
+            os.getenv("POSTGRES_USERNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            database=os.getenv("POSTGRES_DATABASE"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        )
+
+        return conn
+    except:
+        traceback.print_exc()
+        return None
