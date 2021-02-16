961b442733b0c3e9ef7e6b5d0068f5a13bbec1ca
Ryan <ryanjchen2@gmail.com>
2021-02-15 20:23:28 -0800

    Removing print statement

--- tippecanews/utils/matches.py
+++ tippecanews/utils/matches.py
@@ -20,7 +20,6 @@ class User:
 def process_match_request(response: Dict):
     meet_request = True if response["actions"][0]["value"] == "yes" else False
     slack_uid = response["user"]["id"]
-    print(slack_uid)
     conn = get_database_connection()
 
     if meet_request:
